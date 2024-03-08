b00c1c0f45ed9c6eba51ab6c60b8e9ae18aa78fa
SNV <sreenathsnvaltpb@gmail.com>
2024-03-08 10:56:03 +0530

    proj:core::014 : index Page design changed

--- CCTVStorageOpt/settings.py
+++ CCTVStorageOpt/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static/"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -131,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#After 3 days the user will be logged out
+SESSION_COOKIE_AGE = 180
